5b8ff96e3a1182ea3a91f6bde80c20fc0e1950a6
Noosonic <68994083+Noosonic@users.noreply.github.com>
2022-10-09 17:54:03 +0700

    e

--- Demo2/moduleFileDoctor.py
+++ Demo2/moduleFileDoctor.py
@@ -7,8 +7,9 @@ from firebase_admin import credentials, firestore
 fileName = str(date.today())
 fileName = fileName + ".csv"
 
-cred = credentials.Certificate("Demo2/certificate.json")
-app = firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate("Demo2/certificate.json")
+    app = firebase_admin.initialize_app(cred)
 
 store = firestore.client()
 
